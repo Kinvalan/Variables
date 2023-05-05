a12dee9ed44ea9901b2e060759d26a10154690ad
Kinvalan <sebastianmoller01@icloud.com>
2023-05-05 15:02:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿
+/*
+Lag 8 variabler og til-ordne dem en verdi.
+
+Det skal være en variabel per hver type:
+
+int, long, float, decimal, double, string, char og bool
+*/
+
+using System.Diagnostics.CodeAnalysis;
+
+int thisIsAnInt = 3;
+
+long thisIsALong = 4;
+
+float thisIsAFloat = 5.6f;
+
+decimal thisIsADecimal = 3.2M;
+
+double thisIsADouble = 0.0;
+
+string thisIsAString = "hei";
+
+char thisIsAChar = 'a';
+
+bool thisIsABoolean = false;
+
+
+
+/*
+ Lag en konsoll-applikasjon som lager 3 variabler. De kan hete a, b og sum. 
+
+a skal være en int
+b skal være en decimal. 
+Verdiene variablene skal ha er:
+
+a = 5
+b = 3.0 
+sum er a + b.
+
+Hva slags type variabel må sum være?
+*/
+
+
+int a = 5;
+
+decimal b = 3.0M;
+
+decimal sum = a + b;    // Skal være decimal for å beholde desimal-verdien fra b.
+
+
+
+
+
+
+
+
+
+
+
